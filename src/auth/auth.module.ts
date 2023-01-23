c09c9d3fdaf19d6838ae57449939e69256f75f5c
MatheusVict <matheusvictorhenrique@gmail.com>
2023-01-23 19:10:51 -0300

    auth module config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/app/users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      privateKey: process.env.JWT_EXPIRES_TIME,
+      signOptions: { expiresIn: `${process.env.JWT_EXPIRES_TIME}s` },
+    }),
+    PassportModule,
+    UsersModule,
+  ],
+  providers: [AuthService],
 })
 export class AuthModule {}
