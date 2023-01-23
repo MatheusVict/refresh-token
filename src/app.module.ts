76cdd7b466a6a2ff8fd7d21e58803444db040a35
MatheusVict <matheusvictorhenrique@gmail.com>
2023-01-23 16:21:08 -0300

    users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './app/users/users.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       synchronize: true,
       entities: [`${__dirname}/**/*.entity{.js,.ts}`],
     }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
