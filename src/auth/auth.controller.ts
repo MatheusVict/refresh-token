54e34b820415293a62b048e608bbd8235fd23af2
MatheusVict <matheusvictorhenrique@gmail.com>
2023-01-23 20:10:00 -0300

    login token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @UseGuards(AuthGuard('local'))
-  @Post('loin')
-  async login() {
-    return { success: 'YEAH' };
+  @Post('login')
+  async login(@Req() req: Request) {
+    return this.authService.login(req.user);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { UsersEntity } from 'src/app/users/users.entity';
 import { UsersService } from 'src/app/users/users.service';
 import { compareSync } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(user: Partial<UsersEntity>) {
+    const payload = { sub: user.id, email: user.email };
+
+    return {
+      token: this.jwtService.sign(payload),
+    };
+  }
 
   async validateUser(email: string, password: string) {
     let user: UsersEntity;
