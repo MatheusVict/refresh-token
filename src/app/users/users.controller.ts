daaec1ba5836513e78e187f4765924bb68152112
MatheusVict <matheusvictorhenrique@gmail.com>
2023-01-23 16:29:33 -0300

    basic controller

--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get()
+  async index() {}
+
+  @Post()
+  async store() {}
+
+  @Get(':id')
+  async show() {}
+
+  @Put(':id')
+  async update() {}
+
+  @Delete(':id')
+  async destroy() {}
+}
